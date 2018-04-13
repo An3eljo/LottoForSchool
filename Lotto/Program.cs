c78b66c5bd11ea6fa78bfb3f793af482436a7b42
andri.joos69 <andri.joos.tbz@gmail.com>
2018-04-13 14:23:02 +0200

    aj: added Lotto.sln, App.config, Drawing.cs, FillTicket.cs, Lotto.csproj, Program.cs, AssemblyInfo.cs

---
+++ Lotto/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomList = new List<int>();
+            var random = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                randomList.Add(GetRandom(random, randomList));
+            }
+
+
+            foreach (var i in randomList)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+
+        public static int GetRandom(Random random, List<int> randomList)
+        {
+            var r = random.Next(1, 49 + 1);
+            foreach (var i in randomList)
+            {
+                if (i == r)
+                {
+                    r = GetRandom(random, randomList);
+                }
+            }
+
+            return r;
+        }
+    }
+}
